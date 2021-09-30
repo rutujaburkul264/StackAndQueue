aa4781cfd57fcf29fd9c51a7980eb14c3edacdaf
rutujaburkul264 <rutujaburkul264@gmail.com>
2021-09-30 22:38:05 +0530

    Create Stack

---
+++ program_Createstack.cs
@@ -0,0 +1,25 @@
+using StackAndQueue.StackUsingLinkedList;
+using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region StackUsingLinkedList
+
+            Stack linkedListStack = new Stack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            //linkedListStack.Display();
+            //linkedListStack.Peek();
+            //linkedListStack.Pop();
+            //linkedListStack.IsEmpty();
+            //linkedListStack.Display();
+
+            #endregion
+        }
+    }
+}
